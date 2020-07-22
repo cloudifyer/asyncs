20380d3aef2e6defc8ef9e96aa667725468e143e
unknown <srinivas.maroju@hotmail.com>
2020-07-23 00:56:18 +0530

    Demo for RxJS operators - mergeMap & forkJoin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [ BrowserModule, FormsModule, HttpClientModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
